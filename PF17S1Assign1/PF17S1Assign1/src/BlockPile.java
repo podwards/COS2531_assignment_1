ebdd2cf6ad0448e234b5e641664c3dfa95eeba42
Patrick Edwards <patrick.edwards@biarri.com>
2017-04-11 10:07:02 +1000

    removed some unused methods from the blockpile, added comments

--- PF17S1Assign1/PF17S1Assign1/src/BlockPile.java
+++ PF17S1Assign1/PF17S1Assign1/src/BlockPile.java
@@ -18,7 +18,7 @@ public class BlockPile {
 	Block temp;
 	private int height, position;
 	
-	/* 
+	/** 
 	 * This method initialises the pile. It creates a stack with nothing in it
 	 * and so it has a zero height. Because the stack object I've implemented
 	 * can't search for Blocks of a certain height (at least, I couldn't work 
@@ -26,6 +26,8 @@ public class BlockPile {
 	 * certain size, and the stack and sizeStack are kept up to date together
 	 * to allow the stack object to effectively be searched for a block of
 	 * a specified size.
+	 * 
+	 * @param position	the position of the pile (1, 9 or 10).
 	 */
 	
 	public BlockPile(int position)
@@ -36,21 +38,40 @@ public class BlockPile {
 		this.position = position;
 	}
 	
+	/**
+	 * Basically a wrapper for the stack objects own push method. The size stack
+	 * is unfortunately necessary as I can't work out how to get the Stack to search
+	 * using some sort of custom compare method.
+	 * 
+	 * @param block		the block that's getting added to this pile
+	 */
 	public void push(Block block)
 	{
 		this.stack.push(block);
-		this.sizeStack.push(block.getSize()); // TODO: remove this, work out how the search
-										 // method works for Stacks
+		this.sizeStack.push(block.getSize()); /* TODO: remove this, work out how the search
+											     method works for Stacks */
 		this.height += block.getSize();
 		block.setTop(this.height); // whenever a block is added to the stack, it needs this height
 		block.setPosition(this.position);
 	}
-	
+	/**
+	 * Basically a wrapper for the stack objects own peek method. It allows us to check
+	 * what's on top of the pile. Currently unused, consider removing.
+	 */
 	public Block peek()
 	{
 		return this.stack.peek();
 	}
 	
+	/**
+	 * Basically a wrapper for the stack objects own pop method. The size stack
+	 * is unfortunately necessary as I can't work out how to get the Stack to search
+	 * using some sort of custom compare method. For now, both the Block stack and 
+	 * Integer stack need to be used.
+	 * 
+	 * @return temp		the block that was at the top of the pile, and is no longer in the
+	 * 					stack
+	 */
 	public Block pop()
 	{
 		temp = this.stack.pop();
@@ -60,25 +81,36 @@ public class BlockPile {
 		return temp;
 	}
 	
+	
+	/**
+	 * Finds how many spots down the block of a certain size is.
+	 * 
+	 * @param size		the desired size of a block
+	 * @return			an integer. -1 if there's no block of this size, otherwise the 1-based 
+	 * 					position from the top of the stack where the block is located.
+	 */  
 	public int sizeSearch(int size)
 	{
 		return this.sizeStack.search(size);
 	}
 	
-	public boolean empty()
-	{
-		return this.stack.empty();
-	}
-	
+	/**
+	 * Getter method to get the current height of the block
+	 * @return this.height		the current height, it's been updated withs push's and pop's
+	 */
 	public int getHeight()
 	{
 		return this.height;
 	}
 	
-	public int getPosition()
+	/**
+	 * Getter method to get the position of the block
+	 * @return this.height		the current height, it's been updated withs push's and pop's
+	 */
+	/*public int getPosition()
 	{
 		return this.position;
 		
-	}
+	}*/
 
 }
